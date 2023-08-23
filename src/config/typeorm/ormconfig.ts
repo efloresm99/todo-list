c04928f7188a2bc45b3a41b78c54321b156a28f9
Eduardo Flores <ejmartinez@applaudostudios.com>
2023-08-23 17:57:07 -0600

    chore: create typeorm configuration to connect to the database

---
+++ src/config/typeorm/ormconfig.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import { join } from 'path';
+import { DataSource } from 'typeorm';
+
+export const ormconfig = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: false,
+  entities: [join(__dirname, '..', '..', 'entities/*.entity{.ts,.js}')],
+  migrations: [join(__dirname, '..', 'migrations/*{.ts,.js}')],
+});
