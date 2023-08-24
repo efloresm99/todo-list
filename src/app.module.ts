b1278d95ca06e2227897a6b85dfcff5d94b43d37
Eduardo Flores <ejmartinez@applaudostudios.com>
2023-08-23 18:05:02 -0600

    chore: create config path alias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ormconfig } from './config/typeorm';
+
+import { ormconfig } from '@Config/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forRoot(ormconfig.options)],
